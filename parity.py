4aaf3f96bf7a226df27230029a5e8984fad64ff5
Debopriyo Bhowmick <66901287+Debo-Republic@users.noreply.github.com>
2021-02-19 21:48:51 -0500

    compute parity using counting the bits

--- parity.py
+++ parity.py
@@ -1,15 +1,16 @@
 from test_framework import generic_test
-
+# Brute force method to calculate the parity in o(n) time complexity
 
 def parity(x: int) -> int:
-    """Function to count whether a given integer contains even number of 1's. """
-    num_1 = 0
-    while x:
-        num_1 += (x & 1)
-        x >>= 1
-    pair = num_1 % 2
+    """ Logic: Function that counts total number of on bits(1)
+     and returns the remainder when divided by 2"""
+    num_1 = 0  # Initialize counter to 0
+    while x:  # Run the loop until all bits are exhausted
+        num_1 += (x & 1)  # Record if RSB Contains 1
+        x >>= 1  # Right shift to check the next bit
+    pair = num_1 % 2  # Find the parity
     return pair
-   
+
 
 if __name__ == '__main__':
     exit(generic_test.generic_test_main('parity.py', 'parity.tsv', parity))
